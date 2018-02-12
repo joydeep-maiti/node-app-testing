0e9fb1c5bcbafe0ecd925d215453c8cab67cf80f
Joydeep Maiti <joydeep127@gmail.com>
2018-02-12 23:53:36 +0530

    describe block

--- server/server.test.js
+++ server/server.test.js
@@ -1,21 +1,23 @@
 const request = require('supertest');
 const expect = require('expect');
-
 const app = require('./server').app;
-it('should return response', (done)=> {
-    request(app) 
-        .get('/')
-        .expect('Hello World')
-        .end(done)
-})
 
-it('should return users', (done)=> {
-    request(app)
-        .get('/users')
-        .expect({
-            name : 'Joydeep Maiti',
-            age: 21,
-            location: 'kolkata'
-        })
-        .end(done)
+describe('Server', () => {
+    it('should return response', (done) => {
+        request(app)
+            .get('/')
+            .expect('Hello World')
+            .end(done)
+    })
+
+    it('should return users', (done) => {
+        request(app)
+            .get('/users')
+            .expect({
+                name: 'Joydeep Maiti',
+                age: 21,
+                location: 'kolkata'
+            })
+            .end(done)
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,32 +1,34 @@
 var expect = require('expect');
-
 var utils = require('./utils');
 
-it('should square a number',()=> {
-    var res = utils.square(3);
-    // if( res !== 9) {
-    //     throw Error(`res not matched ${res}`);
-    // }
-    expect(res).toBe(9);
-})
-
-it('should split the name',()=> {
-    var user = utils.setName({
-        fullName : 'Joydeep Maiti'
+describe('Utils', ()=> {
+    it('should square a number', () => {
+        var res = utils.square(3);
+        // if( res !== 9) {
+        //     throw Error(`res not matched ${res}`);
+        // }
+        expect(res).toBe(9);
     })
-    // console.log(user);
-    expect(user).toInclude({
-        firstName : 'Joydeep',
-        lastName : 'Maiti'
-    });
-})
 
-it('should add two numbers asynchronously',(done)=> {
-    // console.log('sum1');
-    utils.asyncAdd(2,3, (sum)=> {
-        expect(sum).toBe(5).toBeA('number');
-        // console.log('sum2');
-        done();
+    it('should split the name', () => {
+        var user = utils.setName({
+            fullName: 'Joydeep Maiti'
+        })
+        // console.log(user);
+        expect(user).toInclude({
+            firstName: 'Joydeep',
+            lastName: 'Maiti'
+        });
+    })
+
+    it('should add two numbers asynchronously', (done) => {
+        // console.log('sum1');
+        utils.asyncAdd(2, 3, (sum) => {
+            expect(sum).toBe(5).toBeA('number');
+            // console.log('sum2');
+            done();
+        });
+        // console.log('sum3');
     });
-    // console.log('sum3');
+
 });
